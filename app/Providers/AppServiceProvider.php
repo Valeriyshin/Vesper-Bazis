7d7126e99b2c125016e1164e6e745378d257ce0c
Valeriyshin <valeriy.shin.s@gmail.com>
2026-05-26 00:24:28 +0500

    Use build_b directory for Vite assets

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Vite::useBuildDirectory('build_b');
+
         $newLang = $_GET['lang'] ?? 'ru';
         if(!in_array($newLang, ['ru', 'kk'])) $newLang = 'ru';
         app()->setLocale($newLang);
